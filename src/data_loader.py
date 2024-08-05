9e97394f86ae24e92ae192e760e7c62643a791ad
ppaunovski <paunovskipavel@gmail.com>
2024-08-05 19:00:29 +0200

    gen recipe ingredient df refactor

--- src/data_loader.py
+++ src/data_loader.py
@@ -11,20 +11,19 @@ def load_data_from_json(path_to_json: str) -> list[dict]:
 def get_ingrs_for_recipe(recipe: dict) -> tuple[str, list[str]]:
   return recipe['title'], [ingr['name'] for ingr in recipe['ingredients']]
 
-def gen_recipe_ingredient_dict(data: list[dict]) -> dict[str, list[str]]:
+def gen_recipe_ingredient_data_frame(data: list[dict]) -> dict[str, list[str]]:
    data_dict = {
     'ingredient': [],
     'recipe': []
     }
+   
    for recipe in data:
     recipe_name, ingrs = get_ingrs_for_recipe(recipe)
     for ingr in ingrs:
         data_dict['ingredient'].append(ingr)
         data_dict['recipe'].append(recipe_name)
-    
-    return data_dict
-   
-def gen_recipe_ingredient_data_frame(data: list[dict]) -> dict[str, list[str]]:
-   data_dict = gen_recipe_ingredient_dict(data)
+
    return pd.DataFrame(data_dict)
 
+def csv_file_to_pd_df(path_to_csv: str) -> pd.DataFrame:
+   return pd.read_csv(path_to_csv)
